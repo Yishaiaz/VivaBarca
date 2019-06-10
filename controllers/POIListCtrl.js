839c1bf67d7f2eab8ed40383409d9c5dc72c36c5
yishaia zabary <yz@sky-pick.com>
2019-06-10 09:32:44 +0300

    now with design fixed

--- controllers/POIListCtrl.js
+++ controllers/POIListCtrl.js
@@ -1,19 +1,39 @@
-angular.module("mainApp").controller("POIListCtrl", function($scope, $http) {
-  $http({
-    method: "GET",
-    url: "http://localhost:3000/else/getAllPOIs"
-  }).then(
-    function mySuccess(response) {
-      $scope.allPoisData = response.data["POIs"];
-      $scope.POIS = $scope.allPoisData;
-      console.log($scope.POIS);
-    },
-    function myError(response) {
-      $scope.allPoisData = response.statusText;
-      console.log(response);
-    }
-  );
-});
-function POIClicked(poiDiv) {
-  console.log(poiDiv.id);
-}
+angular
+  .module("mainApp")
+  .controller("POIListCtrl", function($scope, $http, $window) {
+    $scope.bySearchFilter = false;
+    $scope.POIsAfterFiltering = $scope.allPoisData;
+    $http({
+      method: "GET",
+      url: "http://localhost:3000/else/getAllPOIs"
+    }).then(
+      function mySuccess(response) {
+        $scope.allPoisData = response.data["POIs"];
+        $scope.POIS = $scope.allPoisData;
+      },
+      function myError(response) {
+        $scope.allPoisData = response.statusText;
+        console.log(response);
+      }
+    );
+
+    // search function
+    $scope.submitSearch = function() {
+      var searchKeyWord = $scope.searchBox;
+      $scope.POIsAfterFiltering = [];
+      for (let i = 0; i < $scope.allPoisData.length; i++) {
+        if (
+          $scope.allPoisData[i]["name"]
+            .toLowerCase()
+            .includes(searchKeyWord.toLowerCase())
+        ) {
+          $scope.POIsAfterFiltering.push($scope.allPoisData[i]);
+        }
+      }
+      $scope.bySearchFilter = true;
+    };
+
+    $scope.POIClicked = function(event) {
+      $window.location.href = "#!/SinglePOI/" + event.currentTarget.id;
+    };
+  });
--- mainCtrl.js
+++ mainCtrl.js
@@ -30,12 +30,85 @@ mainApp.config(function($routeProvider) {
     controller: "SinglePOICtrl",
     controllerAs: "ctrl"
   });
+  $routeProvider.when("/userFavouritePOIs", {
+    templateUrl: "pages/userFavouritePOIs.html",
+    controller: "userFavouritesCtrl",
+    controllerAs: "ctrl"
+  });
 });
-mainApp.controller("mainCtrl", function($scope) {
-  var userData = localStorage.getItem("userData");
-  if (userData) {
-    console.log(userData);
+mainApp.controller("mainCtrl", function($scope, $http, $window) {
+  // helper functions
+  $scope.goToPoiPage = function(event) {
+    let poiId = event.currentTarget.id;
+    $window.location.href = "#!/SinglePOI/" + poiId;
+  };
+
+  // initializing variables
+  $scope.isLoggedIn = false;
+  $scope.userData = localStorage.getItem("userData");
+  $scope.poisToShowToNewUser = {};
+  $scope.twoPopularPOIS = {};
+  $scope.poisToShowToUser = {};
+
+  // checks user token data exist on local storage
+  if ($scope.userData && $scope.userData != {}) {
+    $scope.userData = JSON.parse($scope.userData);
+    document.getElementById("navUsername").innerText =
+      $scope.userData["username"];
+    $scope.isLoggedIn = true;
+    // twoPopularPOIS
+    // getting two random pois in two different categories to a logged user
+    $http({
+      method: "POST",
+      url: "http://localhost:3000/Analysis/getTwoPOIsByCategories",
+      headers: { "x-auth-token": $scope.userData["token"] }
+    }).then(
+      function mySuccess(response) {
+        $scope.POIData = response.data["POIs"];
+        $scope.twoPopularPOIS = $scope.POIData;
+      },
+      function myError(response) {
+        // $scope.twoPopularPOIS = response.statusText;
+        console.log(response);
+      }
+    );
+    // poisToShowToUser
+    // getting two favourite pois in two different categories to a logged user, if don't exist, changes a scope flag
+    $http({
+      method: "POST",
+      url: "http://localhost:3000/Analysis/getFavoritePOIs",
+      headers: { "x-auth-token": $scope.userData["token"] }
+    }).then(
+      function mySuccess(response) {
+        $scope.POIData = response.data["response"];
+        for (let i = 0; i < $scope.POIData.length; i++) {
+          console.log($scope.POIData[i]["username"]);
+          // todo: change api call to the right one and extract data.
+        }
+      },
+      function myError(response) {
+        console.log("failed");
+      }
+    );
   } else {
-    console.log("no user");
+    document.getElementById("navUsername").innerText = "New User";
+    $scope.isLoggedIn = false;
+    // Getting 3 random points with the minimal rank of 3.5 to present to not logged user.
+    $http({
+      method: "GET",
+      url: "http://localhost:3000/else/getRandomPOI/3/3.5"
+    }).then(
+      function mySuccess(response) {
+        $scope.POIData = response.data["POIs"];
+        $scope.poisToShowToNewUser = $scope.POIData;
+      },
+      function myError(response) {
+        console.log(response);
+      }
+    );
   }
 });
+function logoutUser() {
+  localStorage.setItem("userData", {});
+  location.reload();
+}
